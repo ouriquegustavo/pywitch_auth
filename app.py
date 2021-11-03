9697c2e37f02507fbf7f3d3531a491807b574f76
Gustavo <ourique.gustavo@gmail.com>
2021-11-03 19:13:44 -0300

    Added token generation from code

--- app.py
+++ app.py
@@ -5,20 +5,23 @@ from flask import Flask , request
 
 twitch_client_id = os.environ['CLIENT_ID']
 twitch_client_secret = os.environ['CLIENT_SECRET']
-
+twitch_auth_url = 'https://id.twitch.tv/oauth2/token'
+redirect_uri = 'http://localhost:13486/token'
 
 app = Flask(__name__, '')
 
 @app.route('/')
 def index():
     code = request.args.get('code')
-    response = json.dumps(
-        {
-            "client_id": twitch_client_id, 
-            "client_secret": twitch_client_secret,
-            "code": code,
-        }
-    )
+    params = {
+        'client_id': twitch_client_id
+        'client_secret': twitch_client_secret,
+        'code': code,
+        'grant_type': 'authorization_code'
+        'redirect_uri': redirect_uri
+        
+    }
+    response = request.post(twitch_auth_url, params=params)
     return response
 
 
